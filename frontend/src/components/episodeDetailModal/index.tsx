bb6f161cb3b4ea7319d5596f31b3579952791706
2bit <742005790@qq.com>
2025-01-08 11:41:55 +0800

    feat: 阻止单集详情a标签的默认跳转行为

--- frontend/src/components/episodeDetailModal/index.tsx
+++ frontend/src/components/episodeDetailModal/index.tsx
@@ -6,6 +6,8 @@ import { useDisplayInfo } from '@/hooks'
 import { episodeDetail } from '@/api/episode'
 import './index.moduless.scss'
 import { EpisodeType } from '@/types/episode'
+import { BrowserOpenURL } from 'wailsjs/runtime'
+import { DialogType, ShowMessageDialog } from '@/utils'
 
 type IProps = {
   eid: string
@@ -27,6 +29,30 @@ export const EpisodeDetailModal: React.FC<IProps> = ({
   const [height] = React.useState<number>(useDisplayInfo().Height)
   const [loading, setLoading] = useState<boolean>(false)
   const [detailData, setDetailData] = useState<EpisodeType>()
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const target = event.target as HTMLElement
+    const linkElement = target.closest('a')
+    const href = linkElement?.getAttribute('href')
+
+    /**
+     * 阻止 shownotes 中的 a 标签的默认跳转行为
+     */
+    if (linkElement) {
+      event.preventDefault()
+      if (href) {
+        ShowMessageDialog(
+          DialogType.QUESTION,
+          '跳转提示',
+          `是否在浏览器中打开链接？\r\n${href}`,
+        ).then((res) => {
+          if (res === 'Yes' || res === '是') {
+            BrowserOpenURL(href)
+          }
+        })
+      }
+    }
+  }
 
   /**
    * 获取单集详情
@@ -69,7 +95,10 @@ export const EpisodeDetailModal: React.FC<IProps> = ({
           style={{ height: `${height * 0.7}px` }}
         >
           <div className="episode-detail-modal-wrapper">
-            <div dangerouslySetInnerHTML={{ __html: detailData?.shownotes }} />
+            <div
+              dangerouslySetInnerHTML={{ __html: detailData?.shownotes }}
+              onClick={handleClick}
+            />
           </div>
         </ScrollArea>
       </Spinner>
