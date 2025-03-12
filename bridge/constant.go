b3b3b8d86041fb371a337c8cea0e99224b0fff40
2bit <742005790@qq.com>
2025-03-12 22:45:20 +0800

    release 0.1.0-beta.1

--- bridge/constant.go
+++ bridge/constant.go
@@ -3,6 +3,6 @@ package bridge
 const (
 	APP_NAME                = "horizon"
 	APP_VERSION             = "0.1.0"
-	GITHUB_REPO_RELEASE_URL = "https://api.github.com/repos/ultrazg/test-upgrade/releases/latest"
+	GITHUB_REPO_RELEASE_URL = "https://api.github.com/repos/ultrazg/horizon/releases/latest"
 	DOWNLOAD_ZIPFILE_NAME   = "horizon-upgrade.zip"
 )
