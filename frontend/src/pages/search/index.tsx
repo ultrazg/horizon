db50e96adf199d30fd25fc5b37a7561142444dff
2bit <742005790@qq.com>
2025-07-07 23:37:18 +0800

    save work

--- frontend/src/pages/search/index.tsx
+++ frontend/src/pages/search/index.tsx
@@ -1,26 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
-import {
-  Box,
-  Button,
-  Tabs,
-  TextField,
-  Badge,
-  IconButton,
-  Spinner,
-} from '@radix-ui/themes'
-import {
-  MagnifyingGlassIcon,
-  Cross2Icon,
-  TrashIcon,
-} from '@radix-ui/react-icons'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { Button, Spinner } from '@radix-ui/themes'
 import { search, type searchType } from '@/api/search'
-import { TabPodcast } from './components/tabPodcast'
-import { TabEpisode } from './components/tabEpisode'
-import { TabUser } from './components/tabUser'
 import styles from './index.module.scss'
-import { isEmpty } from 'lodash'
-import { toast, Storage } from '@/utils'
+import { toast } from '@/utils'
 import { ColorfulShadow, ProfileModal } from '@/components'
 import { baseUserType } from '@/types/user'
 import { EpisodeType } from '@/types/episode'
@@ -31,6 +14,7 @@ import { usePlayer } from '@/hooks'
 export const Search: React.FC = () => {
   const location = useLocation()
   const keyword = location.state.keyword
+  const navigateTo = useNavigate()
   const player = usePlayer()
   const [podcastResults, setPodcastResults] = useState<{
     records: PodcastType[]
@@ -60,48 +44,14 @@ export const Search: React.FC = () => {
     open: false,
     uid: '',
   })
-  // const [searchHistory, setSearchHistory] = useState<string[]>([])
 
-  // const saveSearchHistory = (keyword: string) => {
-  //   let history: string[] = Storage.get('search_history') || []
-  //   history = Array.isArray(history) ? [...history] : []
-  //
-  //   history = history.filter((item) => item !== keyword)
-  //
-  //   if (history.length >= 20) {
-  //     history.shift()
-  //   }
-  //
-  //   history.push(keyword)
-  //
-  //   Storage.set('search_history', history.reverse())
-  //
-  //   getSearchHistory()
-  // }
-
-  // const getSearchHistory = () => {
-  //   let history = Storage.get('search_history')
-  //   history = Array.isArray(history) ? [...history] : []
-  //
-  //   setSearchHistory(() => history)
-  // }
-
-  // const clearSearchHistory = () => {
-  //   Storage.remove('search_history')
-  //
-  //   getSearchHistory()
-  // }
-
-  // const removeSearchHistoryKeyword = (keyword: string) => {
-  //   let history: string[] = Storage.get('search_history')
-  //   history = Array.isArray(history) ? [...history] : []
-  //
-  //   history = history.filter((item) => item !== keyword)
-  //
-  //   Storage.set('search_history', history)
-  //
-  //   getSearchHistory()
-  // }
+  const goPodcastDetail = (pid: string) => {
+    navigateTo('/podcast/detail', {
+      state: {
+        pid,
+      },
+    })
+  }
 
   /**
    * 搜索
@@ -118,56 +68,7 @@ export const Search: React.FC = () => {
     }
 
     return await search(params)
-
-    // search(params)
-    //   .then((res) => {
-    //     if (isEmpty(loadMoreKey)) {
-    //       setData({
-    //         records: res.data.data,
-    //         loadMoreKey: res.data?.loadMoreKey,
-    //       })
-    //     } else {
-    //       setData({
-    //         records: [...data.records, ...res.data.data],
-    //         loadMoreKey: res.data?.loadMoreKey,
-    //       })
-    //     }
-    //   })
-    //   .catch(() => {
-    //     toast('搜索失败', { type: 'warn' })
-    //   })
-    //   .finally(() => {
-    //     // saveSearchHistory(params.keyword)
-    //   })
   }
-
-  // const onSearchHistoryClick = (keyword: string) => {
-  //   setSearchParams({
-  //     ...searchParams,
-  //     keyword,
-  //   })
-  //   onSearch(keyword)
-  // }
-
-  // const onChangeHandle = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   setSearchParams({
-  //     ...searchParams,
-  //     keyword: e.target.value,
-  //   })
-  // }
-
-  // const onTabChangeHandle = (value: string) => {
-  //   setSearchParams({
-  //     ...searchParams,
-  //     type: value,
-  //   })
-  // }
-
-  // useEffect(() => {
-  //   if (searchParams.keyword) {
-  //     onSearch()
-  //   }
-  // }, [searchParams.type])
 
   useEffect(() => {
     const onSearchEpisode = async () => {
@@ -250,135 +151,8 @@ export const Search: React.FC = () => {
     onSearchEpisode()
   }, [keyword])
 
-  // useEffect(() => {
-  //   getSearchHistory()
-  // }, [])
-
   return (
     <div className={styles['search-layout']}>
-      {/*<h3>搜索</h3>*/}
-
-      {/*<div className={styles['search-content']}>*/}
-      {/*<div className={styles['search-input']}>*/}
-      {/*  <div className={styles['left']}>*/}
-      {/*    <TextField.Root*/}
-      {/*      size="3"*/}
-      {/*      placeholder="输入关键字"*/}
-      {/*      onChange={onChangeHandle}*/}
-      {/*      value={searchParams.keyword}*/}
-      {/*    >*/}
-      {/*      <TextField.Slot>*/}
-      {/*        <MagnifyingGlassIcon*/}
-      {/*          height="16"*/}
-      {/*          width="16"*/}
-      {/*        />*/}
-      {/*      </TextField.Slot>*/}
-      {/*    </TextField.Root>*/}
-      {/*  </div>*/}
-
-      {/*  <div className={styles['right']}>*/}
-      {/*    <Button*/}
-      {/*      size="3"*/}
-      {/*      onClick={() => {*/}
-      {/*        onSearch()*/}
-      {/*      }}*/}
-      {/*      disabled={!searchParams.keyword}*/}
-      {/*      loading={loading}*/}
-      {/*    >*/}
-      {/*      搜索*/}
-      {/*    </Button>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
-
-      {/*{!isEmpty(searchHistory) && (*/}
-      {/*  <div className={styles['search_history']}>*/}
-      {/*    <div className={styles['search_history_title']}>*/}
-      {/*      <span>搜索历史</span>*/}
-
-      {/*      <IconButton*/}
-      {/*        size="1"*/}
-      {/*        color="gray"*/}
-      {/*        variant="ghost"*/}
-      {/*        style={{ marginLeft: 4 }}*/}
-      {/*        onClick={() => clearSearchHistory()}*/}
-      {/*      >*/}
-      {/*        <TrashIcon*/}
-      {/*          width="14"*/}
-      {/*          height="14"*/}
-      {/*        />*/}
-      {/*      </IconButton>*/}
-      {/*    </div>*/}
-      {/*    {searchHistory.map((item) => {*/}
-      {/*      return (*/}
-      {/*        <Badge*/}
-      {/*          key={item}*/}
-      {/*          color="gray"*/}
-      {/*          style={{ marginRight: 4, cursor: 'pointer' }}*/}
-      {/*        >*/}
-      {/*          <span onClick={() => onSearchHistoryClick(item)}>{item}</span>*/}
-
-      {/*          <IconButton*/}
-      {/*            size="1"*/}
-      {/*            variant="ghost"*/}
-      {/*            onClick={() => removeSearchHistoryKeyword(item)}*/}
-      {/*          >*/}
-      {/*            <Cross2Icon*/}
-      {/*              width="14"*/}
-      {/*              height="14"*/}
-      {/*            />*/}
-      {/*          </IconButton>*/}
-      {/*        </Badge>*/}
-      {/*      )*/}
-      {/*    })}*/}
-      {/*  </div>*/}
-      {/*)}*/}
-
-      {/*<div className={styles['search-result']}>*/}
-      {/*<Tabs.Root*/}
-      {/*  value={searchParams.type}*/}
-      {/*  onValueChange={onTabChangeHandle}*/}
-      {/*>*/}
-      {/*  <Tabs.List size="2">*/}
-      {/*    <Tabs.Trigger value="PODCAST">节目</Tabs.Trigger>*/}
-      {/*    <Tabs.Trigger value="EPISODE">单集</Tabs.Trigger>*/}
-      {/*    <Tabs.Trigger value="USER">用户</Tabs.Trigger>*/}
-      {/*  </Tabs.List>*/}
-
-      {/*  <Box pt="3">*/}
-      {/*    <Tabs.Content value="PODCAST">*/}
-      {/*      <TabPodcast*/}
-      {/*        data={data}*/}
-      {/*        onLoadMore={(loadMoreKey) => {*/}
-      {/*          onSearch('', loadMoreKey)*/}
-      {/*        }}*/}
-      {/*        onRefresh={() => {*/}
-      {/*          onSearch()*/}
-      {/*        }}*/}
-      {/*        loading={loading}*/}
-      {/*      />*/}
-      {/*    </Tabs.Content>*/}
-
-      {/*    <Tabs.Content value="EPISODE">*/}
-      {/*      <TabEpisode*/}
-      {/*        data={data}*/}
-      {/*        onLoadMore={(loadMoreKey) => {*/}
-      {/*          onSearch('', loadMoreKey)*/}
-      {/*        }}*/}
-      {/*        loading={loading}*/}
-      {/*      />*/}
-      {/*    </Tabs.Content>*/}
-
-      {/*    <Tabs.Content value="USER">*/}
-      {/*      <TabUser*/}
-      {/*        data={data}*/}
-      {/*        onLoadMore={(loadMoreKey) => {*/}
-      {/*          onSearch('', loadMoreKey)*/}
-      {/*        }}*/}
-      {/*        loading={loading}*/}
-      {/*      />*/}
-      {/*    </Tabs.Content>*/}
-      {/*  </Box>*/}
-      {/*</Tabs.Root>*/}
       <div className={styles['search-category-title']}>
         <h3>用户</h3>
         <Button variant="ghost">查看更多</Button>
@@ -431,6 +205,9 @@ export const Search: React.FC = () => {
                   <ColorfulShadow
                     src={item.image.picUrl}
                     curPointer
+                    onClick={() => {
+                      goPodcastDetail(item.pid)
+                    }}
                   />
                 </div>
                 <div className={styles['name']}>{item.title}</div>
