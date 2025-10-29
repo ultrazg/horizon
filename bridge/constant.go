84a6a12c9b7233ebb71cf49aa5dd2e6e29a4c7fe
2bit <742005790@qq.com>
2025-10-29 21:58:08 +0800

    bump version to 0.4.1

--- bridge/constant.go
+++ bridge/constant.go
@@ -2,7 +2,7 @@ package bridge
 
 const (
 	APP_NAME                      = "horizon"
-	APP_VERSION                   = "0.4.0"
+	APP_VERSION                   = "0.4.1"
 	GITHUB_REPO_RELEASE_URL       = "https://api.github.com/repos/ultrazg/horizon/releases/latest"
 	DOWNLOAD_ZIPFILE_NAME_WINDOWS = "horizon-windows.zip"
 	DOWNLOAD_ZIPFILE_NAME_MACOS   = "horizon-darwin.zip"
