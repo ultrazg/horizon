a33e35642440fc97e67f05f41ed373e761270cce
2bit <742005790@qq.com>
2026-05-14 15:34:02 +0800

    feat: 查看他人的订阅

--- frontend/src/components/subscriptionModal/index.tsx
+++ frontend/src/components/subscriptionModal/index.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
-import { Modal } from '@/components'
+import React, { useEffect, useState } from 'react'
+import { Spinner, Button } from '@radix-ui/themes'
+import { UpdateIcon, PlusIcon } from '@radix-ui/react-icons'
+import { Modal, Empty, ColorfulShadow } from '@/components'
 import { modalType } from '@/types/modal'
 import { perspectiveType } from '@/types/user'
+import { subscription } from '@/api/subscription'
+import { PodcastType } from '@/types/podcast'
+import { useWindowSize } from '@/hooks'
+import dayjs from 'dayjs'
+import styles from './index.module.scss'
 
 type IProps = {
   uid: string
@@ -14,13 +21,114 @@ export const SubscriptionModal: React.FC<IProps> = ({
   onClose,
   perspective,
 }) => {
+  const width = useWindowSize().width
+  const height = useWindowSize().height * 0.6
+  const [loading, setLoading] = useState<boolean>(false)
+  const [records, setRecords] = useState<PodcastType[]>([])
+  const [loadMoreKey, setLoadMoreKey] = useState<{ skip: number }>({
+    skip: 0,
+  })
+
+  function fetchData(loadMoreKey?: { skip: number }): void {
+    setLoading(true)
+
+    subscription({
+      uid,
+      loadMoreKey,
+    })
+      .then((res) => {
+        setRecords(res.data.data)
+        if (res.data?.loadMoreKey) {
+          setLoadMoreKey({
+            skip: res.data.loadMoreKey.skip,
+          })
+        } else {
+          setLoadMoreKey({
+            skip: 0,
+          })
+        }
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }
+
+  useEffect(() => {
+    if (open) {
+      fetchData()
+    }
+
+    return () => {
+      setRecords([])
+      setLoadMoreKey({
+        skip: 0,
+      })
+    }
+  }, [open])
+
   return (
     <Modal
-      title={`${perspective}的订阅`}
+      title={`${perspective}订阅的节目`}
+      width={`${width * 0.7}px`}
       open={open}
       onClose={onClose}
+      options={
+        loadMoreKey.skip === 0 ? null : (
+          <Button
+            variant="soft"
+            color="gray"
+            onClick={() => {
+              fetchData({ skip: loadMoreKey.skip })
+            }}
+          >
+            <UpdateIcon />
+            加载更多
+          </Button>
+        )
+      }
     >
-      {uid}
+      <Spinner loading={loading}>
+        {records.length === 0 ? (
+          <Empty />
+        ) : (
+          <div
+            style={{
+              maxHeight: height,
+              overflowY: 'scroll',
+              padding: '18px 0 0 18px',
+            }}
+          >
+            {records.map((item) => (
+              <div
+                className={styles['chunk']}
+                key={item.pid}
+              >
+                <div className={styles['left']}>
+                  <ColorfulShadow src={item.image.picUrl} />
+                </div>
+                <div className={styles['middle']}>
+                  <p title={item.title}>{item.title}</p>
+                  <p title={item.description}>{item.description}</p>
+                  <p>
+                    {`${item.subscriptionCount} 人订阅 · ${dayjs(item.latestEpisodePubDate).format('YYYY/MM/DD')} 更新`}
+                  </p>
+                </div>
+                <div className={styles['right']}>
+                  <Button
+                    variant="soft"
+                    color={
+                      item.subscriptionStatus === 'ON' ? 'gray' : undefined
+                    }
+                  >
+                    {item.subscriptionStatus === 'ON' ? null : <PlusIcon />}
+                    {item.subscriptionStatus === 'ON' ? '已订阅' : '订阅'}
+                  </Button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </Spinner>
     </Modal>
   )
 }
