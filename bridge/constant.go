ec00d99ba567d92ed18f7eca4c40a5c717835599
2bit <742005790@qq.com>
2026-06-02 00:01:51 +0800

    bump version to 0.7.4

--- bridge/constant.go
+++ bridge/constant.go
@@ -2,7 +2,7 @@ package bridge
 
 const (
 	APP_NAME                      = "horizon"
-	APP_VERSION                   = "0.7.3"
+	APP_VERSION                   = "0.7.4"
 	GITHUB_REPO_RELEASE_URL       = "https://api.github.com/repos/ultrazg/horizon/releases/latest"
 	CHANGELOG_URL                 = "https://raw.githubusercontent.com/ultrazg/horizon/main/CHANGELOG.md"
 	DOWNLOAD_ZIPFILE_NAME_WINDOWS = "horizon-windows.zip"
