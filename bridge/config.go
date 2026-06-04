ba19f3a2c1d0d1c5689a4af0c460a06b65f3a869
2bit <742005790@qq.com>
2026-06-04 11:15:22 +0800

    fix(config): 优化配置初始化逻辑

--- bridge/config.go
+++ bridge/config.go
@@ -38,22 +38,14 @@ func initConfig() {
 	viper.SetConfigFile(configFile)
 	viper.SetConfigType("yaml")
 
+	setDefaults()
+
 	if _, err := os.Stat(configFile); os.IsNotExist(err) {
-		setDefaults()
-
-		err := viper.ReadInConfig()
-		if err != nil {
-			log.Printf("读取配置文件失败: %v", err)
-
-			if err = viper.SafeWriteConfigAs(configFile); err != nil {
-				log.Printf("写入配置文件失败: %v", err)
-			}
+		if err := viper.SafeWriteConfigAs(configFile); err != nil {
+			log.Printf("写入配置文件失败: %v", err)
 		}
-	} else {
-		err := viper.ReadInConfig()
-		if err != nil {
-			log.Printf("读取配置文件失败: %v", err)
-		}
+	} else if err := viper.ReadInConfig(); err != nil {
+		log.Printf("读取配置文件失败: %v", err)
 	}
 }
 
